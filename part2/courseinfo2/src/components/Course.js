6211e9a5a098ee7d753c5b016a1dc3922a07a4c1
Eduard Peters <eduardpeters@MacBook-Air.local>
2022-04-25 22:43:21 +0200

    Adding completed courseinfo2, exercises 2.1 to 2.5

---
+++ part2/courseinfo2/src/components/Course.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Total = ({ parts }) => {
+    let initialValue = 0
+    const total = parts.reduce((previousValue, currentValue) => {
+      return previousValue + currentValue.exercises
+    }, initialValue)
+    return (
+      <p><b>total of {total} exercises</b></p>
+    )
+}
+  
+const Part = ({ part }) => {
+    return (
+        <p>
+        {part.name} {part.exercises}
+        </p>
+    )
+}
+  
+const Course = ({ course }) => {
+    return (
+        <div>
+            <h1>{course.name}</h1>
+            {course.parts.map(part =>
+            <Part key={part.id} part={part} />
+            )}
+            <Total parts={course.parts} />
+        </div>
+    )
+}
+
+export default Course
